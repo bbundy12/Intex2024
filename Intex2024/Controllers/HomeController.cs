30f2845e17ff2abbd9e8cf292781596669a2be9b
jacobbigler <jacob.bigler03@gmail.com>
2024-04-10 15:37:56 -0600

    At least the page shows up

--- Intex2024/Controllers/HomeController.cs
+++ Intex2024/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Intex2024.Data;
+using Intex2024.Infrastructure;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,13 +11,49 @@ namespace Intex2024.Controllers
     {
         private IIntexRepository _repo;
         private readonly UserManager<Customer> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public HomeController(IIntexRepository repo, UserManager<Customer> userManager)
+        public HomeController(IIntexRepository repo, UserManager<Customer> userManager, IHttpContextAccessor httpContextAccessor)
         {
             _repo = repo;
             _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
         }
 
+        public IActionResult Cart()
+        {
+            Cart cart = _httpContextAccessor.HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return View(cart);
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(int productId, int quantity)
+        {
+            Product product = _repo.Products.FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                Cart cart = _httpContextAccessor.HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+                cart.AddItem(product, quantity);
+                _httpContextAccessor.HttpContext.Session.SetJson("Cart", cart);
+            }
+            return RedirectToAction("Cart");
+        }
+
+        [HttpPost]
+        public IActionResult RemoveFromCart(int productId)
+        {
+            Product product = _repo.Products.FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                Cart cart = _httpContextAccessor.HttpContext.Session.GetJson<Cart>("Cart");
+                if (cart != null)
+                {
+                    cart.RemoveLine(product);
+                    _httpContextAccessor.HttpContext.Session.SetJson("Cart", cart);
+                }
+            }
+            return RedirectToAction("Cart");
+        } 
 
         public IActionResult Index()
         {
@@ -60,11 +97,6 @@ namespace Intex2024.Controllers
                 }
             };
             return View(vm);
-        }
-
-        public IActionResult Cart()
-        {
-            return View();
         }
 
         public IActionResult ProductDetails()
