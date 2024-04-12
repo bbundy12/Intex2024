2c0e8af75c31239cec93eb7d1ec37fde98ec9c68
jacobbigler <jacob.bigler03@gmail.com>
2024-04-11 21:56:28 -0600

    Added the product recommendation functionality

--- Intex2024/Pages/ProductDetails.cshtml.cs
+++ Intex2024/Pages/ProductDetails.cshtml.cs
@@ -3,6 +3,7 @@ using Intex2024.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Build.Evaluation;
+using Microsoft.EntityFrameworkCore;
 
 namespace Intex2024.Pages
 {
@@ -14,16 +15,34 @@ namespace Intex2024.Pages
             _repo = temp;
         }
         public Product product { get; set; }
+        public List<Product> RecommendedProducts { get; set; }
+
         public void OnGet(int id)
         {
-            product = new Product();
             product = _repo.Products
                     .FirstOrDefault(x => x.ProductId == id);
+
+            if (product != null)
+            {
+                // Get all RecommendedProductIds for the clicked product
+                var recommendedProductIds = _repo.ProductRecommendations
+                                            .Where(pr => pr.ProductId == id)
+                                            .Select(pr => pr.RecommendedProductId)
+                                            .ToList();
+
+                if (recommendedProductIds.Any())
+                {
+                    // Fetch all recommended products
+                    RecommendedProducts = _repo.Products
+                                            .Where(p => recommendedProductIds.Contains(p.ProductId))
+                                            .ToList();
+                }
+            }
         }
         public void OnPost()
         {
             
         }
-        
-    }
+
+}
 }
